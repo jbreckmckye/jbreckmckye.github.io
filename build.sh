a28735f0d5aa54a11675eb505f502d5934115475
Jimmy Breck-McKye <jimmy@breck-mckye.com>
2020-05-20 18:56:25 +0100

    Wrong theme config filename was blowing up CI

--- build.sh
+++ build.sh
@@ -2,10 +2,6 @@
 
 mkdir themes
 git clone https://github.com/jbreckmckye/hexo-theme-octo.git themes/octo
-cp _config.theme.yml ./themes/octo/config.yml
-
-echo "INSIDE OCTO:"
-cd ./themes/octo && ls
-echo "-----"
+cp _config.theme.yml ./themes/octo/_config.yml
 
 hexo generate
