c5167352a7ff9228eb2c5d05dc40483cbce8cf88
Jimmy Breck-McKye <jimmy@breck-mckye.com>
2020-05-20 18:46:58 +0100

    [CI] wrong way around

--- build.sh
+++ build.sh
@@ -3,8 +3,9 @@
 mkdir themes
 git clone https://github.com/jbreckmckye/hexo-theme-octo.git themes/octo
 cp _config.theme.yml ./themes/octo/config.yml
-hexo generate
 
 echo "INSIDE OCTO:"
 cd ./themes/octo && ls
 echo "-----"
+
+hexo generate
